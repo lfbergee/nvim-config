685c127a22a4add37a2af42b1fc46aa318c36d22
Leiv Fredrik Berge <leiv.fredrik.berge@mattilsynet.no>
2023-11-20 13:52:16 +0100

    update

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.filetype.add({
+  extension = {
+    mdx = "mdx",
+  },
+})
+
+vim.treesitter.language.register("markdown", "mdx")
